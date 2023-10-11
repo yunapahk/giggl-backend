777e80683ff1830246b98ac9e9612f3a719941c0
Yuna Pahk <yunapahk@gmail.com>
2023-10-11 16:19:15 -0700

    comedians model updated

--- comedians/models.py
+++ comedians/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Comedian(models.Model):
     name = models.CharField(max_length=200)
-    profile_picture = models.ImageField(upload_to='comedian_pics/', null=True)
+    profile_picture = models.TextField()
