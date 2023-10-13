b417b499a13ba98e7cb4a8eac98273c5a88a7c01
Yuna Pahk <yunapahk@gmail.com>
2023-10-13 11:38:46 -0700

    podcasts model updated to include iframe

--- podcasts/models.py
+++ podcasts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
 class Podcast(models.Model):
     name = models.CharField(max_length=200)
     comedians = models.CharField(max_length=200)
+    youtube_video_id = models.CharField(max_length=40, blank=True, null=True) # new field
